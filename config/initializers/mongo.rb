a7efbf0014eb60a3eb64709ecca46c9ef66d7123
nicolasMachut <machut.nicolas@live.fr>
2015-03-19 11:32:12 +0100

    mongo

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -6,8 +6,8 @@ db_config = YAML::load(File.read(File.join(Rails.root, "/config/mongo.yml")))
 #
 if db_config[Rails.env] && db_config[Rails.env]['adapter'] == 'mongodb'
   mongo = db_config[Rails.env]
-  MongoMapper.connection = Mongo::Connection.new(mongo['host'] || 'localhost',
-                                                 mongo['port'] || 27017,
+  MongoMapper.connection = Mongo::Connection.new(mongo['host'] || 'ds053778.mongolab.com',
+                                                 mongo['port'] || 53778,
                                                  :logger => Rails.logger)
   MongoMapper.database = mongo['database']
 

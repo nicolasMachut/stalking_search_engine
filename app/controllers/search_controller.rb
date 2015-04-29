837735e02490a501305145d1979d823344fedd74
nicolasMachut <machut.nicolas@live.fr>
2015-04-29 12:12:21 +0200

    commit yeah

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,9 +5,6 @@ class SearchController < ApplicationController
   def search
     keyword = [params[:keyWords]][0]
 
-    response = open('https://api.github.com/users?since=135').read
-    puts JSON.parse(response)[0].login
-
     render json: find(keyword)
   end
 
